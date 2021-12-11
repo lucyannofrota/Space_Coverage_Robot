89690d1b732bba6ad9d3fbb1e193e26eb204851a
Lucyanno Frota <lucyannofrota@gmail.com>
2021-12-11 02:00:14 +0000

    gridMap overlay rviz

--- src/space_coverage/src/SC_planner.hpp
+++ src/space_coverage/src/SC_planner.hpp
@@ -1,19 +1,27 @@
-// #include <iostream>
-// #include <nav_msgs/
 
 enum SC_CELL_TYPE{OCC = 1,FREE = 0};
-// struct id_ref{
-//     int32_t id;
-//     int16_t row;
-//     int16_t col;
-// };
+
+struct grid_cord{
+    uint16_t i;
+    uint16_t j;
+    grid_cord(void){
+        this->i = 0;
+        this->j = 0;
+    }
+    grid_cord(uint16_t i_,uint16_t j_){
+        this->i = i_;
+        this->j = j_;
+    }
+};
 
 class SC_planner{
 
     private:
     float cell_size_m;
     uint16_t cell_size_pix;
-    cv::Mat_<uint8_t> map;
+    cv::Mat_<uchar> gridMap; //0 - Invalid Cell, 1 - Free Cell, 2 - Occ Cell
+    cv::Mat_<uchar> *baseMap;
+    float baseMap_resolution;
 
     ros::Publisher *pub; // Will publish if defined
     visualization_msgs::Marker marker;
@@ -25,6 +33,8 @@ class SC_planner{
 
     bool initialized_markers;
 
+    unsigned int mapExtremes[4];
+
     // id_ref marker_id;
 
     public:
@@ -33,41 +43,23 @@ class SC_planner{
 
         this->cell_size_pix = 0;
 
-        // this->map = cv::Mat::zeros(ceil(msg.info.height/cell_size_pix),ceil(msg.info.width/cell_size_pix),CV_8UC1);
+        // this->gridMap = cv::Mat::zeros(ceil(msg.info.height/cell_size_pix),ceil(msg.info.width/cell_size_pix),CV_8UC1);
 
         this->pub = NULL; 
 
-        this->c_occ.a = 1.0;
-        this->c_occ.r = 0.5;
-        this->c_occ.g = 1.0;
-        this->c_occ.b = 0.5;
+        this->c_occ.a = 0.2;
+        this->c_occ.r = 0.0;
+        this->c_occ.g = 0.5;
+        this->c_occ.b = 0.0;
 
-        this->c_free = this->c_occ;
-        this->c_free.g = 0.5;
+        this->c_free.a = 0.2;
+        this->c_free.r = 80.0/255.0;
+        this->c_free.g = 80.0/255.0;
+        this->c_free.b = 1.0;
 
         this->initialized_markers = false;
     }
-    /*SC_planner(float cell_m,const nav_msgs::OccupancyGrid& msg, const cv::Mat_<uint8_t> *base_map, ros::Publisher &publisher){
-        this->cell_size_m = cell_m;
-        // std::cout << "Csize: " << this->cell_size_m << std::endl;
-        // std::cout << "Resolution: " << msg.info.resolution << std::endl;
-        this->cell_size_pix = ceil(this->cell_size_m*(1/msg.info.resolution));
 
-        // this->map = new 
-
-        // cv::Size()
-        // std::cout << "Size: " << floor(msg.info.height/this->cell_size_pix) << std::endl;
-        std::cout << "Cell_size_pix: " << this->cell_size_pix << std::endl;
-        std::cout << "H: " << floor(msg.info.height/cell_size_pix) << std::endl;
-        std::cout << "W: " << floor(msg.info.width/cell_size_pix) << std::endl;
-
-        this->map = cv::Mat::zeros(ceil(msg.info.height/cell_size_pix),ceil(msg.info.width/cell_size_pix),CV_8UC1);
-
-        std::cout << "Starting SC_Planner" << std::endl;
-        std::cout << this->map.size() << std::endl;
-
-        this->pub = NULL;
-    };*/
     ~SC_planner(){
         
     };
@@ -76,30 +68,66 @@ class SC_planner{
         return this->cell_size_pix;
     }
 
-    void setup_planner(const nav_msgs::OccupancyGrid& msg/*const cv::Mat_<uint8_t> *base_map*/){
-
+    void setup_planner(const nav_msgs::OccupancyGrid& msg, unsigned int *mapExtremes, cv::Mat_<uchar> &base_map, float map_res){
+        
+        this->baseMap_resolution = map_res;
+        this->baseMap = &base_map;
         this->cell_size_pix = ceil(this->cell_size_m*(1/msg.info.resolution));
 
-        this->map = cv::Mat::zeros(ceil(msg.info.height/cell_size_pix),ceil(msg.info.width/cell_size_pix),CV_8UC1);
+        // printf("%f = ceil(%u,%u) \n",ceil(msg.info.width/cell_size_pix),msg.info.width,cell_size_pix);
 
-        // // cv::MatSize temp = this->map.size;
-        // // this->map.
-        
-        // // (this->map).rows()
+        uint16_t height = (int)floor((float) msg.info.height/cell_size_pix);
+        uint16_t width = (int)floor((float) msg.info.width/cell_size_pix);
 
-        // std::cout << "Map h: " << this->map.rows << std::endl;
-        // std::cout << "Map w: " << this->map.cols << std::endl;
+        this->gridMap = cv::Mat::zeros(height,width,CV_8UC1);
+
+        std::cout << "Cell Size: " << this->cell_size_pix << std::endl;
+        std::cout << "Map h: " << this->gridMap.rows << std::endl;
+        std::cout << "Map w: " << this->gridMap.cols << std::endl;
+
+        this->mapExtremes[0] = mapExtremes[0];
+        this->mapExtremes[1] = mapExtremes[1];
+        this->mapExtremes[2] = mapExtremes[2];
+        this->mapExtremes[3] = mapExtremes[3];
+
+        // Fill the grid
+        for(uint16_t i = 0; i < this->gridMap.rows; i++){
+            for(uint16_t j = 0; j < this->gridMap.cols; j++){
+                bool free = true;
+                for(uint16_t cy = 0; cy < this->cell_size_pix; cy++){
+                    uint16_t y = cy+i*this->cell_size_pix;
+                    if(y > this->baseMap->rows || y < this->mapExtremes[2] || y > this->mapExtremes[3]){
+                        this->gridMap.at<uchar>(i,j) = 0;
+                        goto label1;
+                    }
+                    for(uint16_t cx = 0; cx < this->cell_size_pix; cx++){
+                        uint16_t x = cx+j*this->cell_size_pix;
+                        if(x > this->baseMap->cols || x < this->mapExtremes[0] || x > this->mapExtremes[1]){
+                            this->gridMap.at<uchar>(i,j) = 0;
+                            goto label1;
+                        }
+
+                        if(this->baseMap->at<uchar>(y,x) <= 200) free = false;
+                    }
+                }
+                if(free) this->gridMap.at<uchar>(i,j) = 1;
+                else this->gridMap.at<uchar>(i,j) = 0;
+                label1:
+                continue;
+            }
+        }
+
     }
 
     void define_pub(ros::Publisher &publisher){
         this->pub = &publisher;
 
-        
+        // Base marker definition
         this->marker.header.frame_id = "map";
         this->marker.header.stamp = ros::Time();
         this->marker.ns = "SC_Planner";
         this->marker.id = 0;
-        this->marker.type = visualization_msgs::Marker::CUBE;
+        this->marker.type = visualization_msgs::Marker::CUBE_LIST;
         this->marker.action = visualization_msgs::Marker::ADD;
         this->marker.pose.position.x = 0;
         this->marker.pose.position.y = 0;
@@ -110,14 +138,10 @@ class SC_planner{
         this->marker.pose.orientation.w = 1.0;
         this->marker.scale.x = this->cell_size_m;
         this->marker.scale.y = this->cell_size_m;
-        this->marker.scale.z = 0.001;
-        this->marker.color.a = 1; // Don't forget to set the alpha!
-        this->marker.color.r = 1.0;
-        this->marker.color.g = 0.0;
-        this->marker.color.b = 0.0;
-        // this->marker.lifetime.fromSec(0.15);
+        this->marker.scale.z = 0.01;
+        // this->marker.color.a = 1.0; // Don't forget to set the alpha!
+        this->marker.lifetime.fromSec(0.15);
     }
-
 
     bool validDisplacement(geometry_msgs::Point pose){
         //TODO
@@ -129,71 +153,93 @@ class SC_planner{
         return true;
     }
 
-
     bool iterate(const geometry_msgs::Point &current_pose,geometry_msgs::Point &new_pose){
         //TODO
         if(!this->initialized_markers){
             this->initialize_markers();
         }
+        this->pub->publish(this->marker);
         return true;
     }
 
+    geometry_msgs::Point transform_grid_to_world(grid_cord cord){
+        //TODO introduzir o valor da TF dinamico
+        geometry_msgs::Point out_pose;
 
-    void transform_grid_to_map(void){
-        //TODO
+        // TF comp
+        out_pose.x += -1.0;
+        out_pose.y += -1.0;
+
+        // Center offset
+        out_pose.x += this->marker.scale.x/2;
+        out_pose.y += this->marker.scale.y/2;
+
+        // Grid offset
+        //BUG O tamanho de celula interfere na componente de overlap de escala
+        out_pose.x += cord.j*(this->marker.scale.x+this->baseMap_resolution/4); //   /2 para 0.6
+        out_pose.y += cord.i*(this->marker.scale.y+this->baseMap_resolution/4);
+        return out_pose;
     }
 
-    void setMarker(bool new_marker, int32_t id, SC_CELL_TYPE type, geometry_msgs::Pose &pose){
-        if(new_marker) this->marker.action = visualization_msgs::Marker::ADD;
-        else this->marker.action = visualization_msgs::Marker::MODIFY;
-        this->marker.id = id;
-        if(type) this->marker.color = this->c_occ;
-        else this->marker.color = this->c_free;
-        this->marker.pose = pose;
+    grid_cord transform_world_to_grid(const geometry_msgs::Point point){
+        //TODO introduzir o valor da TF dinamico
+        geometry_msgs::Point temp = point;
+        grid_cord cord_out = grid_cord();
+
+        // TF comp
+        temp.x -= -1.0;
+        temp.y -= -1.0;
+
+        // Center offset
+        temp.x -= this->marker.scale.x/2;
+        temp.y -= this->marker.scale.y/2;
+
+        // Grid offset
+        cord_out.j = temp.x/(this->marker.scale.x+this->baseMap_resolution/4);
+        cord_out.i = temp.y/(this->marker.scale.y+this->baseMap_resolution/4);
+        // out_pose.x += j*this->marker.scale.x;
+        // out_pose.y += i*this->marker.scale.y;
+
+        return cord_out;
     }
 
+    // void setMarker(bool new_marker, int32_t id, SC_CELL_TYPE type, const geometry_msgs::Pose &pose){
+    //     if(new_marker) this->marker.action = visualization_msgs::Marker::ADD;
+    //     else this->marker.action = visualization_msgs::Marker::MODIFY;
+    //     this->marker.id = id;
+    //     if(type) this->marker.color = this->c_occ;
+    //     else this->marker.color = this->c_free;
+    //     this->marker.pose = pose;
+    // }
 
     void initialize_markers(void){
-        geometry_msgs::Pose pose;
-        for(uint16_t i = 0; i < this->map.rows; i++){
-            for(uint16_t j = 0; j < this->map.cols; j++){
-                // TODO set markers positons
-                this->setMarker(true,this->id_ref(i,j),SC_CELL_TYPE::FREE,pose); // ID's limitados a mapas pequenos
-                this->pub->publish(this->marker);
+        geometry_msgs::Point pose;
+    
+        for(uint16_t i = 0; i < this->gridMap.rows; i++){
+            for(uint16_t j = 0; j < this->gridMap.cols; j++){
+                pose = this->transform_grid_to_world(grid_cord(i,j));
+                pose.z = 0.05;
+                switch(this->gridMap.at<uchar>(i,j)){
+                    case 0:
+                        // this->marker.points.push_back(pose);
+                        // this->marker.colors.push_back(this->c_free);
+                        break;
+                    case 1:
+                        this->marker.points.push_back(pose);
+                        this->marker.colors.push_back(this->c_free);
+                        break;
+                    case 2:
+                        this->marker.points.push_back(pose);
+                        this->marker.colors.push_back(this->c_occ);
+                        break;
+                }
+                // if(this->gridMap.at<uchar>(i,j) == 0;
             }
         }
         this->initialized_markers = true;
     }
 
-    int32_t id_ref(uint16_t i, uint16_t j){
-        return i*10000+j;
-    }
+    void editMarker(){
 
-    // void createMarker(void){
-    //     visualization_msgs::Marker marker;
-    //     marker.header.frame_id = "map";
-    //     marker.header.stamp = ros::Time();
-    //     marker.ns = "SC_Planner";
-    //     marker.id = id;
-    //     marker.type = visualization_msgs::Marker::CUBE;
-    //     marker.action = visualization_msgs::Marker::ADD;
-    //     marker.pose.position.x = 0;
-    //     marker.pose.position.y = 0;
-    //     marker.pose.position.z = 0;
-    //     marker.pose.orientation.x = 0.0;
-    //     marker.pose.orientation.y = 0.0;
-    //     marker.pose.orientation.z = 0.0;
-    //     marker.pose.orientation.w = 1.0;
-    //     marker.scale.x = this->cell_size_m;
-    //     marker.scale.y = this->cell_size_m;
-    //     marker.scale.z = 0.001;
-    //     marker.color.a = 1; // Don't forget to set the alpha!
-    //     marker.color.r = 0.0;
-    //     marker.color.g = 1.0;
-    //     marker.color.b = 0.0;
-    //     marker.lifetime.fromSec(0.15);
-    //     //only if using a MESH_RESOURCE marker type:
-    //     // marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
-    //     // this->pub->publish( marker );
-    // }
+    }
 };
--- src/space_coverage/src/space_coverage_planner.cpp
+++ src/space_coverage/src/space_coverage_planner.cpp
@@ -11,14 +11,16 @@
 
 #include <visualization_msgs/Marker.h>
 
-const float cell_size_m = (0.225*2.0)*(3.0/5.0); // 2* Robot radius
+const float cell_size_m = (0.225*2.0)*(0.7); // 2* Robot radius
 const float occ_threshold = 0.5;
 
 #include "SC_planner.hpp"
+#include <stdio.h>
 
 static SC_planner *planner = NULL;
 static geometry_msgs::Point new_pose;
-static cv::Mat_<uint8_t> *map = NULL;
+static cv::Mat_<uchar> *map = NULL;
+static unsigned int mapExtremes[4]; // [min_x,max_x,min_y,max_y]
 // static cv::Mat_<uint8_t> *gridOverlay = NULL;
 
 // static ros::Publisher vis_pub;
@@ -32,107 +34,132 @@ static cv::Mat_<uint8_t> *map = NULL;
 
 void drawCallback(void){
     try{
-        cv::imshow("Space Coverage Planner", *map);
-        cv::waitKey(1);
+        if(map != NULL){
+            // cv::imshow("Space Coverage Planner", *map);
+            // cv::waitKey(1);
+        }
     }
-    catch(...){
-        std::cout << "Error" << std::endl;
+    catch(.../*cv::Exception ex*/){
+        std::cout << "drawCallback error!" << std::endl;
+        // std::cout << ex.msg << std::endl;
     }
 }
 
 void odomCallback(const nav_msgs::Odometry& msg){
-    std::cout << "x: " << msg.pose.pose.position.x 
+    try{
+        std::cout << "x: " << msg.pose.pose.position.x 
               << "\ty: " << msg.pose.pose.position.y
               << "\tz: " << msg.pose.pose.position.z
               << std::endl;
-    if(planner!=NULL){
-        if(planner->iterate(msg.pose.pose.position,new_pose)){
-            // new_pose is valid!
+        if(planner!=NULL){
+            if(planner->iterate(msg.pose.pose.position,new_pose)){
+                // new_pose is valid!
+                std::cout << "planner iteration" << std::endl;
+            }
         }
+        // cell_size
+        drawCallback();
     }
-    // cell_size
-    drawCallback();
+    catch (...){
+        std::cout << "odomCallback error!" << std::endl;
+    }
 }
 
 void initializeMap(const nav_msgs::OccupancyGrid& msg){
     // Code based on:
     //      https://github.com/ROBOTIS-GIT/humanoid_navigation/blob/master/gridmap_2d/src/GridMap2D.cpp
+    try{
+        std::vector<signed char>::const_iterator mapDataIter = msg.data.begin();
 
-    std::vector<signed char>::const_iterator mapDataIter = msg.data.begin();
+        map = new cv::Mat_<uchar>;
 
-    map = new cv::Mat_<uint8_t>;
+        *map = cv::Mat::zeros(msg.info.height,msg.info.width,CV_8UC1);
 
-    *map = cv::Mat::zeros(msg.info.height,msg.info.width,CV_8UC1);
-
-    for(int i = msg.info.height; i >= 0; i--){
-        for(int j = 0; j < msg.info.width; j++){
-            if(*mapDataIter == -1){
-                map->at<uchar>(i,j) = 127;
+        for(int i = 0; i < msg.info.height; i++){
+            for(int j = 0; j < msg.info.width; j++){
+                if(*mapDataIter == -1){
+                    map->at<uchar>(i,j) = 127;
+                }
+                else{
+                    if(*mapDataIter >= occ_threshold) map->at<uchar>(i,j) = 0;
+                    else map->at<uchar>(i,j) = 255;
+                }
+                ++mapDataIter;
             }
-            else{
-                if(*mapDataIter >= occ_threshold) map->at<uchar>(i,j) = 0;
-                else map->at<uchar>(i,j) = 255;
-            }
-            // map->at<uchar>(i,j) = 127-(255/100)*(*mapDataIter);
-            ++mapDataIter;
         }
+
+        mapExtremes[0] = 9999;
+        mapExtremes[1] = 0;
+        mapExtremes[2] = 9999;
+        mapExtremes[3] = 0;
+
+        for(int i = 0; i < msg.info.height; i++){
+            for(int j = 0; j < msg.info.width; j++){
+                if(map->at<uchar>(i,j) == 255){
+                    if(j < mapExtremes[0]) mapExtremes[0] = j;
+                    if(j > mapExtremes[1]) mapExtremes[1] = j;
+                    if(i < mapExtremes[2]) mapExtremes[2] = i;
+                    if(i > mapExtremes[3]) mapExtremes[3] = i;
+                }
+            }
+        }
+        printf("X: [%u,%u], Y: [%u,%u]\n",mapExtremes[0],mapExtremes[1],mapExtremes[2],mapExtremes[3]);
+    }
+    catch (...) {
+        std::cout << "initializeMap function error!" << std::endl;
     }
 
-
-
-
-
-    // TODO Fazer crop da imagem. E resolver a transformada em outra escala e que relaciona um ponto na imagem com o mapa
 }
 
-// void initializeGridOverlay(void){
-//     // gridOverlay
-//     // *gridOverlay = cv::Mat::zeros(msg.info.height,msg.info.width,CV_8UC1);
-// }
-
 void mapCallBack(const nav_msgs::OccupancyGrid& msg){
-    
-    if(map == NULL){
-        initializeMap(msg);
-        planner->setup_planner(msg);
+    try{
+        if(map == NULL){
+            initializeMap(msg);
+            planner->setup_planner(msg,mapExtremes,*map,msg.info.resolution);
+        }
+
+
+        
+        // cv::imshow("Space Coverage Planner", img0);
+        // cv::waitKey(1);
+        drawCallback();
     }
-
-
-    
-    // cv::imshow("Space Coverage Planner", img0);
-	// cv::waitKey(1);
-    drawCallback();
+    catch (...){
+        std::cout << "mapCallback error!" << std::endl;
+    }
 }
 
 int main(int argc, char **argv){
-	ros::init(argc, argv, "SC_Planner");
-	ros::NodeHandle nh;
+    try{
+        ros::init(argc, argv, "SC_Planner");
+        ros::NodeHandle nh;
 
-    cv::namedWindow("Space Coverage Planner");
+        cv::namedWindow("Space Coverage Planner");
 
-    planner = new SC_planner(cell_size_m);
+        planner = new SC_planner(cell_size_m);
 
-    ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "SC_Planner", 0 );
+        ros::Publisher vis_pub = nh.advertise<visualization_msgs::Marker>( "SC_Planner", 0 );
 
-    planner->define_pub(vis_pub);
-
-
-
+        planner->define_pub(vis_pub);
 
 
+        ros::Subscriber subs_odom = nh.subscribe("/odom", 10, odomCallback);
+        ros::Subscriber subs_map = nh.subscribe("/map", 3, mapCallBack);
 
-    ros::Subscriber subs_odom = nh.subscribe("/odom", 10, odomCallback);
-    ros::Subscriber subs_map = nh.subscribe("/map", 3, mapCallBack);
+        ros::Rate rate(10);
+        while(ros::ok()){
 
-	ros::Rate rate(10);
-	while(ros::ok()){
+            // planner->iterate();
+        //     // publishMarkers(vis_pub);
 
-        // planner->iterate();
-        // publishMarkers(vis_pub);
+            rate.sleep();
+            ros::spinOnce();
+        }
+    }
+    catch (...){
+        std::cout << "main function error!" << std::endl;
+    }
 
-		rate.sleep();
-		ros::spinOnce();
-	}
 }
 
 
